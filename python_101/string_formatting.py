aeecd1a4416005c547a5853e154e304c498180cb
Maryanemwende <maryanemwende333@gmail.com>
2023-06-06 15:37:55 +0000

    Exploring more string formatting styles

--- python_101/string_formatting.py
+++ python_101/string_formatting.py
@@ -8,6 +8,22 @@ year = int(input("When did you graduate from highschool? : "))
 
 #let's print that out using the old formatting method
 print("You attended %s in %s and finished in %d" %(high_school, city, year))
+print()
 
 #let's print using str.format method of formatting
 print("You attended {} in {} and finished in {}".format(high_school, city, year))
+print()
+
+#An alternative to type casting
+print("You attended {:s} in {:s} and finished in {:d}".format(high_school, city, year))
+print()
+
+#specifying the indices while printing the string
+print("You attended {0} in {2} and finished in {1}".format(high_school, city, year))
+print()
+
+#using the newest method of string formatting f-string
+print(f"You attended {high_school} in {city} and finished in {year}")
+print()
+print(F"You attended {high_school} in {city} and finished in {year}")
+

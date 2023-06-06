50a455581b4ca578cec01afc5b5074f8874eb319
Maryanemwende <maryanemwende333@gmail.com>
2023-06-06 15:15:18 +0000

    testing string formatting methods

--- python_101/string_formatting.py
+++ python_101/string_formatting.py
@@ -2,9 +2,12 @@
 
 #This program explores a method (%) of formatting strings
 
-High_School = input("Which HighSchool did you attend? : ")
+high_school = input("Which HighSchool did you attend? : ")
 city = input("In which city was your highschool located? : ")
 year = int(input("When did you graduate from highschool? : "))
 
-#let's print that out
-print("You attended %s in %s and finished in %d" %(High_School, city, year))
+#let's print that out using the old formatting method
+print("You attended %s in %s and finished in %d" %(high_school, city, year))
+
+#let's print using str.format method of formatting
+print("You attended {} in {} and finished in {}".format(high_school, city, year))

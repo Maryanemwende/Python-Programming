e993b6f80514c17afc4e9587d2d2fed5cb17d88e
Maryanemwende <maryanemwende333@gmail.com>
2023-06-06 13:44:57 +0000

    Worked with keywords and the input function

--- python_101/print_func.py
+++ python_101/print_func.py
@@ -6,3 +6,13 @@ my_surname = "Musyoka"
 print(my_name, my_surname, end=" ")
 print("stays at ", end=" ")
 print("Nairobi", end="\n")
+
+#let's explore sep keyword
+print(my_name, my_surname, sep="_")
+
+#let's look at input
+my_sis = input("I have a twin and her name is: ")
+our_age = input("We are: ")
+
+print("My twin's name and age respectively: ", end=" ")
+print(my_sis, our_age)

ad3eb51adc3d64d089502ca420c368b5afe58e82
Maryanemwende <maryanemwende333@gmail.com>
2023-06-06 12:52:20 +0000

    Exploring the print function

---
+++ python_101/print_func.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+my_name = "Maryane"
+my_surname = "Musyoka"
+
+#This program prints my name
+print(my_name, my_surname, end=" ")
+print("stays at ", end=" ")
+print("Nairobi", end="\n")

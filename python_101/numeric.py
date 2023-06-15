e1a2b74000e780a9d8dca748f829e3472183dcac
Maryanemwende <maryanemwende333@gmail.com>
2023-06-15 12:50:14 +0000

    numbers in python

---
+++ python_101/numeric.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+num = 10
+digit = 10.5
+
+print(type(num))
+print(type(digit))
+
+print(10 + 10) #addition
+print(10 * 10) #multiplication
+print(10 / 5) #division
+print(10 / 3)
+print(10 // 3) #floor division
+print(10 ** 4) #exponentiation(to the power of)
+print(10 % 4) #modulus (remainder)
+
+#in-built functions to work with numbers
+print(abs(-35))
+print(round(4.44)) #round off to nearest integer
+print(round(4.45, 1)) #round off to first digit after decimal

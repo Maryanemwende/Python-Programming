9242e6ea3d75dad0eaf68c1f09358ea8151a543c
Maryanemwende <maryanemwende333@gmail.com>
2023-06-15 11:09:46 +0000

    tackled two methods of function definition

--- python_functions/def_func.py
+++ python_functions/def_func.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 #define your function
+
 def calculate_my_age():
     name = "Maryane Mwende"
     year_of_birth = 1999
@@ -13,6 +14,7 @@ calculate_my_age()
 #to be more practical , let's pass parameters to the function
 #the parameters passed to the function are used to calculate the user's age
 #parameters make it possible for the function to be utilised by anyone, anytime
+
 def whats_my_age(my_name, year_of_birth, current_year):
     my_age = current_year - year_of_birth
     print("{} you are {} years old".format(my_name, my_age))
